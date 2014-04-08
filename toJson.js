da0349d848219e7645c3ca332173efbb692ed59b
Thomas Davis <thomasalwyndavis@gmail.com>
2014-04-08 14:38:00 +1000

    Wrote a script to generate json for congress forms

---
+++ toJson.js
@@ -0,0 +1,38 @@
+var fs = require('fs');
+var csv = require('csv');
+var _ = require('lodash');
+// opts is optional
+var opts = {};
+
+csv()
+.from.path(__dirname+'/7- output-final.csv', { delimiter: ',', escape: '"' })
+.to.array( function(rows){
+  // Remove header information from array
+  rows.shift();
+  var legislators = {};
+  _.each(rows, function(row) {
+    /*[ '1726 Kingsley Avenue',
+    'Orange Park',
+    'FL',
+    '32073',
+    '4411',
+    'FL',
+    '3',
+    'Ted Yoho',
+    'Y000065' ]*/
+    var legislator_id = row[8]
+    var info = {
+      example_address: row[0],
+      example_city: row[1],
+      example_state: row[2],
+      zip5: row[3],
+      zip4: row[4],
+      example_state: row[5],
+      example_district: row[6]
+
+    }
+    legislators[legislator_id] = info;
+  })
+
+  fs.writeFileSync('legislators.json', JSON.stringify(legislators, null, 4), 'utf8')
+});
